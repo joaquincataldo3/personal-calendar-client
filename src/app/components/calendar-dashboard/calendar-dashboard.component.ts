479725368057e1d73d853d3973155ea7ea3e67ac
joaquincataldo3 <jqncataldo@gmail.com>
2025-06-09 08:38:55 -0300

    add monthly calendar and import in dashboard

--- src/app/components/calendar-dashboard/calendar-dashboard.component.ts
+++ src/app/components/calendar-dashboard/calendar-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WeeklyCalendarComponent } from '../weekly-calendar/weekly-calendar.component';
+import { MonthlyCalendarComponent } from '../monthly-calendar/monthly-calendar.component';
 
 @Component({
   selector: 'app-calendar-dashboard',
   standalone: true,
-  imports: [WeeklyCalendarComponent],
+  imports: [WeeklyCalendarComponent, MonthlyCalendarComponent],
   templateUrl: './calendar-dashboard.component.html',
   styleUrl: './calendar-dashboard.component.css'
 })
---
+++ src/app/components/monthly-calendar/monthly-calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-calendar',
+  standalone: true,
+  imports: [],
+  templateUrl: './monthly-calendar.component.html',
+  styleUrl: './monthly-calendar.component.css'
+})
+export class MonthlyCalendarComponent {
+
+}
