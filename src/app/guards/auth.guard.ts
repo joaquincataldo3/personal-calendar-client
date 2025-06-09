e5a44fb02cb4c624faa73c55227cfca0c5b64728
joaquincataldo3 <jqncataldo@gmail.com>
2025-06-08 22:14:56 -0300

    add auth guard logic for authentication

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { catchError, map, of } from 'rxjs';
+import { IApiResponse } from '../../interfaces/interfaces';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.checkAuth().pipe(
+    map((res: IApiResponse) => {
+      return true;
+    }),
+    catchError(() => {
+      router.navigate(['/sign-in']);
+      return of(false);
+    })
+  );
 };
