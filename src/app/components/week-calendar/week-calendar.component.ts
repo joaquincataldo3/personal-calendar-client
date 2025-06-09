d6e23d2123bfce8b922798066355fc518e927151
joaquincataldo3 <jqncataldo@gmail.com>
2025-06-09 13:04:09 -0300

    week calendar component renaming

--- src/app/components/week-calendar/week-calendar.component.ts
+++ src/app/components/week-calendar/week-calendar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-weekly-calendar',
+  selector: 'app-week-calendar',
   standalone: true,
   imports: [],
-  templateUrl: './weekly-calendar.component.html',
-  styleUrl: './weekly-calendar.component.css'
+  templateUrl: './week-calendar.component.html',
+  styleUrl: './week-calendar.component.css'
 })
-export class WeeklyCalendarComponent {
+export class WeekCalendarComponent {
 
 }
