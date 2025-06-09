5186bc2ec92f2e0b13f2c698cc228ddda411debb
joaquincataldo3 <jqncataldo@gmail.com>
2025-06-09 20:07:23 -0300

    add logic to check for selected day in weekly calendar

--- src/app/components/week-calendar/week-calendar.component.ts
+++ src/app/components/week-calendar/week-calendar.component.ts
@@ -128,4 +128,12 @@ export class WeekCalendarComponent {
       return Math.max(percent, minPercent);
     }
 
+    isSelectedDay(date1: Date, date2: Date): boolean {
+      return (
+        date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear()
+      );
+    }
+
 }
