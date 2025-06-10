23a27056ab1616846bb48543cbe1323ca0821f02
joaquincataldo3 <jqncataldo@gmail.com>
2025-06-09 21:02:51 -0300

    add angular material and edit event modal component

---
+++ src/app/components/edit-event-modal/edit-event-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-event-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-event-modal.component.html',
+  styleUrl: './edit-event-modal.component.css'
+})
+export class EditEventModalComponent {
+
+}
