00ae2f75b07f2b5837d232f11ad15b021fc56480
joaquincataldo3 <jqncataldo@gmail.com>
2025-06-09 22:03:51 -0300

    add basic form for edit event

--- src/app/components/edit-event-modal/edit-event-modal.component.ts
+++ src/app/components/edit-event-modal/edit-event-modal.component.ts
@@ -2,26 +2,45 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { IEvent } from '../../../interfaces/interfaces';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-event-modal',
   standalone: true,
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, ReactiveFormsModule],
   templateUrl: './edit-event-modal.component.html',
   styleUrl: './edit-event-modal.component.css'
 })
 export class EditEventModalComponent {
 
   eventToEdit!: IEvent;
+  eventForm!: FormGroup;
 
  constructor(
     public dialogRef: MatDialogRef<EditEventModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: IEvent
+    @Inject(MAT_DIALOG_DATA) public data: IEvent,
+    private fb: FormBuilder
   ) {
     this.eventToEdit = data;
+    this.eventForm = this.fb.group({
+      title: [data.title, Validators.required],
+      description: [data.description],
+      start_time: [data.start_time, Validators.required],
+      end_time: [data.end_time, Validators.required]
+    });
   }
-  
+
   onCancel() {
     this.dialogRef.close();
   }
+
+  onDelete() {
+
+  }
+
+  onEditEvent() {
+
+  }
+
+
 }
